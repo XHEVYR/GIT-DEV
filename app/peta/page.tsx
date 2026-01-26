12c1ec892acbf92e451e60752ad8670df2da6b6e
Rafli Fattah Nur Ikhsan <133248578+RafliF1@users.noreply.github.com>
2026-01-26 09:54:44 +0700

    fix: change nav from kediri to blitar

--- app/peta/page.tsx
+++ app/peta/page.tsx
@@ -7,7 +7,7 @@ const Map = dynamic(() => import('@/components/map'), { ssr: false });
 export default function PetaPage() {
   return (
     <div className="h-screen w-full flex flex-col">
-      <nav className="bg-blue-600 p-4 text-white font-bold">GIS KOTA KEDIRI</nav>
+      <nav className="bg-blue-600 p-4 text-white font-bold">GIS KOTA BLITAR</nav>
       <div className="flex-1">
         <Map />
       </div>
